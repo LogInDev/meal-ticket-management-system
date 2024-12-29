b0eaa1aeecb0b67cbe4775d57ba2bc42cfcfb664
LogInDev <fhazlt303@gmail.com>
2024-12-29 10:25:39 +0900

    gitignore, db세팅

---
+++ src/main/java/com/nivuskorea/mealticketmanagement/MealTicketManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nivuskorea.mealticketmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealTicketManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealTicketManagementApplication.class, args);
+    }
+
+}
