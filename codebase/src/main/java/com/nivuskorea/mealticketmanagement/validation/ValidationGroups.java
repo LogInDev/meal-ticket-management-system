a0c6c1dd3d2de1d98c52b59ccbff356eec59a25d
LogInDev <fhazlt303@gmail.com>
2025-02-08 16:30:52 +0900

    추가 및 수정되는 사용자 정보 유효성 검증 및 검증 순서 설정

---
+++ codebase/src/main/java/com/nivuskorea/mealticketmanagement/validation/ValidationGroups.java
@@ -0,0 +1,6 @@
+package com.nivuskorea.mealticketmanagement.validation;
+
+public class ValidationGroups {
+    public interface NotBlankGroup {}   // 공백 검증 그룹
+    public interface PatternGroup {}    // 숫자 확인 그룹
+}
